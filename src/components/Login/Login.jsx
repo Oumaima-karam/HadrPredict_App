d9e6f054035526046a160c437f9387c894a3a057
oumaima karam <oumaimakaram5@gmail.com>
2025-04-29 00:33:59 +0200

    migrate from CRA to Vite

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -4,50 +4,50 @@ import logo from "../Assets/logoo.png";
 import { Link } from "react-router-dom";
 
 const Login = () => {
-  return (
+return (
     <div className="login-container">
-      <div className="login-header">
+    <div className="login-header">
         <img src={logo} alt="logo" />
-      </div>
-      <div className="inputs">
+    </div>
+    <div className="inputs">
         <div className="input">
-          <label htmlFor="email">Email :</label>
-          <input
+        <label htmlFor="email">Email :</label>
+        <input
             type="email"
             id="email"
             // value={email}
             // onChange={(e) => setEmail(e.target.value)}
             // required
-          />
+        />
         </div>
         <div className="input">
-          <label htmlFor="password">Mot de passe :</label>
-          <input
+        <label htmlFor="password">Mot de passe :</label>
+        <input
             type="password"
             id="password"
             // value={password}
             // onChange={(e) => setPassword(e.target.value)}
             // required
-          />
+        />
         </div>
-      </div>
+    </div>
 
-      <div className="form-options">
+    <div className="form-options">
         <label className="remember-me">
-          <input
+        <input
             type="checkbox"
             id="rememberMe"
             // checked={rememberMe}
             // onChange={(e) => setRememberMe(e.target.checked)}
-          />
-          <span className="checkmark"></span>
-          Se souvenir de moi
+        />
+        <span className="checkmark"></span>
+        Se souvenir de moi
         </label>
         <Link to="/forgot-password">Mot de passe oublié ?</Link>
-      </div>
-      <div className="submit-container">Login</div>
     </div>
-  );
+    <div className="submit-container">Login</div>
+    </div>
+);
 };
 
 export default Login;
