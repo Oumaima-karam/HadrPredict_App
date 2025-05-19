7d56a772960bf61d8494c6f03733f0073715dcdd
ibtissamBenarba <ibtissambenarba03@gmail.com>
2025-05-19 21:15:18 +0100

    singup and login backend

---
+++ src/main/java/com/ensao/hadrpredictapi/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.ensao.hadrpredictapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // <- ici on met le pattern d'URL de l'API à exposer
+                        .allowedOrigins("http://localhost:3000") // origine autorisée
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
